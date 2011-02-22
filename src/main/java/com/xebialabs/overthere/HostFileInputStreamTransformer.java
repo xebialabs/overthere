8ea8808c71c10db2b84499a25ec41dbc73363399
hierynomus <jeroen@hierynomus.com>
2011-02-22 09:11:21 +0100

    Added License on top of all the files using the Gradle License Plugin

--- src/main/java/com/xebialabs/overthere/HostFileInputStreamTransformer.java
+++ src/main/java/com/xebialabs/overthere/HostFileInputStreamTransformer.java
@@ -1,4 +1,20 @@
 /*
+ * This file is part of Overthere.
+ * 
+ * Overthere is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Overthere is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Overthere.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
  * Copyright (c) 2008-2010 XebiaLabs B.V. All rights reserved.
  *
  * Your use of XebiaLabs Software and Documentation is subject to the Personal
@@ -42,3 +58,4 @@ public interface HostFileInputStreamTransformer {
 	InputStream transform(HostFile input);
 
 }
+
--- src/main/java/com/xebialabs/overthere/ssh/SshSftpHostFile.java
+++ src/main/java/com/xebialabs/overthere/ssh/SshSftpHostFile.java
@@ -1,3 +1,19 @@
+/*
+ * This file is part of Overthere.
+ * 
+ * Overthere is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Overthere is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Overthere.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.xebialabs.overthere.ssh;
 
 import java.io.InputStream;
@@ -251,3 +267,4 @@ class SshSftpHostFile extends SshHostFile {
 	private static Logger logger = LoggerFactory.getLogger(SshSftpHostFile.class);
 
 }
+
