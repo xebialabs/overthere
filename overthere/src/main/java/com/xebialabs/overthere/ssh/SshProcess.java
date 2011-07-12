120e59bae57a9b215b415378ab0b71c149845bc6
Vincent Partington <vpartington@xebialabs.com>
2011-07-12 10:37:03 +0200

    Revert "- No longer sending SIGKILL when destroying the process: it was causing spurious exceptions when a command would fail. Which is often: The 'ls -ld /tmp/deployit-1234567890.tmp' executed when a connection is created usually fails."
    This reverts commit d802cc870bfd28e1472903e30bb396adc0652636.

--- overthere/src/main/java/com/xebialabs/overthere/ssh/SshProcess.java
+++ overthere/src/main/java/com/xebialabs/overthere/ssh/SshProcess.java
@@ -6,6 +6,7 @@ import java.io.OutputStream;
 import net.schmizz.sshj.common.SSHException;
 import net.schmizz.sshj.connection.ConnectionException;
 import net.schmizz.sshj.connection.channel.direct.Session;
+import net.schmizz.sshj.connection.channel.direct.Signal;
 import net.schmizz.sshj.transport.TransportException;
 
 import org.slf4j.Logger;
@@ -62,7 +63,13 @@ class SshProcess implements OverthereProcess {
 
     @Override
     public void destroy() {
-	    closeSession();
+	    try {
+		    command.signal(Signal.KILL);
+	    } catch (TransportException e) {
+		    logger.warn("Could not send the KILL signal to the command, closing the session.", e);
+	    } finally {
+		    closeSession();
+	    }
     }
 
 	private void closeSession() {
