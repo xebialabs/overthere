xebialabs/overthere

commit 329a27b45caac8ea45717e1f8d07862a1a6722bd

Read the body of response message when HTTP response code is >=400 (server reported an error).

overthere/src/main/java/com/xebialabs/overthere/cifs/winrm/connector/JdkHttpConnector.java
@@ -78,7 +78,14 @@ public class JdkHttpConnector implements HttpConnector {
 			bw.flush();
 			bw.close();
 
-			InputStream is = urlConnection.getInputStream();
+			InputStream is;
+			if (con.getResponseCode() >= 400) {
+			     /* Read error response */
+			    is = httpConn.getErrorStream();
+			} else {
+			    is = httpConn.getInputStream();
+			}
+			
 			Writer writer = new StringWriter();
 			try {
 				int n;

commit 4cc987eb1d51c090ebfe9b7b52a0b8db783e0264

Fixed compiler error introduced by commit 329a27b45caac8ea45717e1f8d07862a1a6722bd

overthere/src/main/java/com/xebialabs/overthere/cifs/winrm/connector/JdkHttpConnector.java
@@ -81,9 +81,9 @@ public class JdkHttpConnector implements HttpConnector {
 			InputStream is;
 			if (con.getResponseCode() >= 400) {
 			     /* Read error response */
-			    is = httpConn.getErrorStream();
+			    is = con.getErrorStream();
 			} else {
-			    is = httpConn.getInputStream();
+			    is = con.getInputStream();
 			}
 			
 			Writer writer = new StringWriter();