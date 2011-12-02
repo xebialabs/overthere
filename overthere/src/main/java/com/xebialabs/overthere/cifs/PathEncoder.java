e21a52eda942e76b9d379aee2cf3dfb93c600a7e
Andrew Phillips <demobox1@yahoo.co.uk>
2011-12-02 18:00:47 -0500

    Extracted SMB path encoding logic into PathEncoder

--- overthere/src/main/java/com/xebialabs/overthere/cifs/PathEncoder.java
+++ overthere/src/main/java/com/xebialabs/overthere/cifs/PathEncoder.java
@@ -1,13 +1,69 @@
 package com.xebialabs.overthere.cifs;
 
+import static com.xebialabs.overthere.cifs.CifsConnectionBuilder.DEFAULT_CIFS_PORT;
+import static java.lang.String.format;
+import static java.util.regex.Pattern.quote;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import com.xebialabs.overthere.RuntimeIOException;
+
 /**
  * Conversions to/from UNC, SMB and Windows file paths
  */
 class PathEncoder {
+    private final String smbUrlPrefix;
+
+    PathEncoder(String username, String password, String address, int cifsPort) {
+        StringBuilder urlPrefix = new StringBuilder();
+        urlPrefix.append("smb://");
+        urlPrefix.append(urlEncode(username.replaceFirst(quote("\\"), ";")));
+        urlPrefix.append(":");
+        urlPrefix.append(urlEncode(password));
+        urlPrefix.append("@");
+        urlPrefix.append(urlEncode(address));
+        if (cifsPort != DEFAULT_CIFS_PORT) {
+            urlPrefix.append(":");
+            urlPrefix.append(cifsPort);
+        }
+        urlPrefix.append("/");
+        this.smbUrlPrefix = urlPrefix.toString();
+    }
+
+    private static String urlEncode(String value) {
+        try {
+            return URLEncoder.encode(value, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeIOException("Unable to construct SMB URL", e);
+        }
+    }
+
+    String toSmbUrl(String hostPath) {
+        if (hostPath.length() < 2) {
+            throw new IllegalArgumentException(format("Host path '%s' is too short", hostPath));
+        }
+
+        if (hostPath.charAt(1) != ':') {
+            throw new IllegalArgumentException(format("Host path '%s' does not have a colon (:) as its second character", hostPath));
+        }
+
+        StringBuilder smbUrl = new StringBuilder(smbUrlPrefix);
+        smbUrl.append(hostPath.charAt(0));
+        smbUrl.append("$/");
+        if (hostPath.length() >= 3) {
+            if (hostPath.charAt(2) != '\\') {
+                throw new IllegalArgumentException(format("Host path '%s' does not have a backslash (\\) as its third character", hostPath));
+            }
+            smbUrl.append(hostPath.substring(3).replace('\\', '/'));
+        }
+        return smbUrl.toString();
+    }
 
     /**
-     * @param uncPath   the UNC path to convert to a Windows file path
-     * @return  the Windows file path representing the UNC path
+     * @param uncPath
+     *            the UNC path to convert to a Windows file path
+     * @return the Windows file path representing the UNC path
      */
     static final String fromUncPath(String uncPath) {
         int slashPos = uncPath.indexOf('\\', 2);
--- overthere/src/test/java/com/xebialabs/overthere/cifs/PathEncoderTest.java
+++ overthere/src/test/java/com/xebialabs/overthere/cifs/PathEncoderTest.java
@@ -1,5 +1,6 @@
 package com.xebialabs.overthere.cifs;
 
+import static com.xebialabs.overthere.cifs.CifsConnectionBuilder.DEFAULT_CIFS_PORT;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -9,9 +10,91 @@ import org.junit.Test;
  * Unit tests for the {@link PathEncoder}
  */
 public class PathEncoderTest {
+    PathEncoder encoder;
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void hostPathOfLessThanTwoCharsFails() {
+        encoder = new PathEncoder("user", "pass", "windows-box", DEFAULT_CIFS_PORT);
+        encoder.toSmbUrl("c");
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void requiresColonAsSecondChar() {
+        encoder = new PathEncoder("user", "pass", "windows-box", DEFAULT_CIFS_PORT);
+        encoder.toSmbUrl("c!");
+    }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void charAfterColonMustBeSlash() {
+        encoder = new PathEncoder("user", "pass", "windows-box", DEFAULT_CIFS_PORT);
+        encoder.toSmbUrl("c:c");
+    }
+    
     @Test
-    public void extractsDriveAndPath() {
+    public void replacesFirstSlashInUsername() {
+        encoder = new PathEncoder("domain\\user", "pass", "windows-box", DEFAULT_CIFS_PORT);
+        // %3B = ;
+        assertThat(encoder.toSmbUrl("c:\\Temp"), is("smb://domain%3Buser:pass@windows-box/c$/Temp"));
+    }
+    
+    @Test
+    public void urlEncodesUsername() {
+        encoder = new PathEncoder("us;er", "pass", "windows-box", DEFAULT_CIFS_PORT);
+        // %3B = ;
+        assertThat(encoder.toSmbUrl("c:\\Temp"), is("smb://us%3Ber:pass@windows-box/c$/Temp"));
+    }
+    
+    @Test
+    public void urlEncodesPassword() {
+        encoder = new PathEncoder("user", "pa;ss", "windows-box", DEFAULT_CIFS_PORT);
+        // %3B = ;
+        assertThat(encoder.toSmbUrl("c:\\Temp"), is("smb://user:pa%3Bss@windows-box/c$/Temp"));
+    }
+    
+    @Test
+    public void urlEncodesAddress() {
+        encoder = new PathEncoder("user", "pass", "windows;box", DEFAULT_CIFS_PORT);
+        // %3B = ;
+        assertThat(encoder.toSmbUrl("c:\\Temp"), is("smb://user:pass@windows%3Bbox/c$/Temp"));
+    }
+    
+    @Test
+    public void ignoresDefaultCifsPort() {
+        encoder = new PathEncoder("user", "pass", "windows-box", DEFAULT_CIFS_PORT);
+        // %3B = ;
+        assertThat(encoder.toSmbUrl("c:\\Temp"), is("smb://user:pass@windows-box/c$/Temp"));
+    }
+    
+    @Test
+    public void addsNonDefaultCifsPortToUrl() {
+        encoder = new PathEncoder("user", "pass", "windows-box", 0);
+        // %3B = ;
+        assertThat(encoder.toSmbUrl("c:\\Temp"), is("smb://user:pass@windows-box:0/c$/Temp"));
+    }
+    
+    @Test
+    public void usesAdminShareForDrive() {
+        encoder = new PathEncoder("user", "pass", "windows-box", DEFAULT_CIFS_PORT);
+        // %3B = ;
+        assertThat(encoder.toSmbUrl("c:\\"), is("smb://user:pass@windows-box/c$/"));
+    }
+    
+    @Test
+    public void addsTrailingSlashIfMissing() {
+        encoder = new PathEncoder("user", "pass", "windows-box", DEFAULT_CIFS_PORT);
+        // %3B = ;
+        assertThat(encoder.toSmbUrl("c:"), is("smb://user:pass@windows-box/c$/"));
+    }
+    
+    @Test
+    public void usesForwardSlashAsFileSeparator() {
+        encoder = new PathEncoder("user", "pass", "windows;box", 0);
+        // %3B = ;
+        assertThat(encoder.toSmbUrl("c:\\Temp\\file.txt"), is("smb://user:pass@windows%3Bbox:0/c$/Temp/file.txt"));
+    }
+    
+    @Test
+    public void extractsDriveAndPathFromUncPath() {
         assertThat(PathEncoder.fromUncPath("\\\\windows-box\\c$\\Temp\\file.txt"), 
                 is("c:\\Temp\\file.txt"));
     }
