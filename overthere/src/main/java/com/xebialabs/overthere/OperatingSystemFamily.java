1caf6a9102a302586df6fab454165306ff34f810
Vincent Partington <vpartington@xebialabs.com>
2012-01-12 11:00:55 +0100

    Rewrote OperatingSystemFamily enum to use instance variables instead of if-statements to return OS-specific values.

--- overthere/src/main/java/com/xebialabs/overthere/OperatingSystemFamily.java
+++ overthere/src/main/java/com/xebialabs/overthere/OperatingSystemFamily.java
@@ -24,100 +24,49 @@ import java.io.StringReader;
  * The family (flavour) of the operating system running on a host.
  */
 public enum OperatingSystemFamily {
+	
 	/**
 	 * An operating system from the Windows family: NT, XP, Server 2003, Vista, etc.
 	 */
-	WINDOWS,
+	WINDOWS('\\', ';', "\r\n", "&&", ".bat", "C:\\windows\\temp"),
 
 	/**
 	 * An operating system from the Unix family: Linux, AIX, MacOS, etc.
 	 */
-	UNIX;
+	UNIX('/', ':', "\n", ";", ".sh", "/tmp");
+
+	private final String fileSeparator;
+	
+	private final char fileSeparatorChar;
+
+	private final String pathSeparator;
+
+	private final char pathSeparatorChar;
+
+	private final String lineSeparator;
+
+	private final String commandSeparator;
+
+	private final String scriptExtension;
+
+	private final String defaultTemporaryDirectoryPath;
+
+	private OperatingSystemFamily(char fileSeparatorChar, char pathSeparatorChar, String lineSeparator, String commandSepator, String scriptExtension, String defaultTemporaryDirectoryPath) {
+		this.scriptExtension = scriptExtension;
+		this.lineSeparator = lineSeparator;
+		this.defaultTemporaryDirectoryPath = defaultTemporaryDirectoryPath;
+		this.fileSeparator = String.valueOf(fileSeparatorChar);
+		this.fileSeparatorChar = fileSeparatorChar;
+		this.pathSeparator = String.valueOf(pathSeparatorChar);
+		this.pathSeparatorChar = pathSeparatorChar;
+		this.commandSeparator = commandSepator;
+	}
 
 	/**
 	 * Returns the {@link OperatingSystemFamily} that corresponds to the local host
 	 */
 	public static OperatingSystemFamily getLocalHostOperatingSystemFamily() {
 		return System.getProperty("os.name").startsWith("Windows") ? WINDOWS : UNIX;
-	}
-
-	/**
-	 * Returns the extension for scripts used by the operating system family, e.g. <tt>.bat</tt> or <tt>.sh</tt>
-	 * 
-	 * 
-	 * @return the script extension including the preceding dot
-	 */
-	public String getScriptExtension() {
-		if (this == WINDOWS) {
-			return ".bat";
-		} else {
-			return ".sh";
-		}
-	}
-
-	/**
-	 * Returns the characters used by the operating system family to separate line in a text file, e.g. <tt>\r\n</tt> or <tt>\n</tt>
-	 * 
-	 * @return the line separator
-	 */
-	public String getLineSeparator() {
-		if (this == WINDOWS) {
-			return "\r\n";
-		} else {
-			return "\n";
-		}
-	}
-
-	/**
-	 * Returns the default path of the temporary directory for this operating system family, i.e. <tt>C:\temp</tt> or <tt>/tmp</tt>.
-	 * 
-	 * @return the path
-	 */
-	public String getDefaultTemporaryDirectoryPath() {
-		if (this == WINDOWS) {
-			return "C:\\windows\\temp";
-		} else {
-			return "/tmp";
-		}
-	}
-
-	/**
-	 * Returns the character used by the operating system family to separate components of a file path, e.g. <tt>\</tt> or <tt>/<tt>.
-	 * 
-	 * @return the file separator.
-	 */
-	public String getFileSeparator() {
-		if (this == WINDOWS) {
-			return "\\";
-		} else {
-			return "/";
-		}
-	}
-
-	/**
-	 * Returns the character used by the operating system family to separate components of a path, e.g. <tt>;</tt> or <tt>:<tt>.
-	 * 
-	 * @return the file separator.
-	 */
-	public String getPathSeparator() {
-		if (this == WINDOWS) {
-			return ";";
-		} else {
-			return ":";
-		}
-	}
-
-	/**
-	 * Returns the character used by the operating system family to separate commands in a command line, e.g. <tt>&&</tt> or <tt>;<tt>.
-	 * 
-	 * @return the command separator.
-	 */
-	public String getCommandSeparator() {
-		if (this == WINDOWS) {
-			return "&&";
-		} else {
-			return ";";
-		}
 	}
 
 	/**
@@ -144,6 +93,78 @@ public enum OperatingSystemFamily {
 		} catch (IOException exc) {
 			throw new RuntimeIOException("Unable to read String", exc);
 		}
+	}
+
+	/**
+	 * Returns the string used by the operating system family to separate components of a file path, e.g. <tt>\</tt> or <tt>/<tt>.
+	 * 
+	 * @return the file separator.
+	 */
+	public String getFileSeparator() {
+		return fileSeparator;
+	}
+
+	/**
+	 * Returns the character used by the operating system family to separate components of a file path, e.g. <tt>\</tt> or <tt>/<tt>.
+	 * 
+	 * @return the file separator.
+	 */
+	public char getFileSeparatorChar() {
+		return fileSeparatorChar;
+	}
+
+	/**
+	 * Returns the string used by the operating system family to separate components of a path, e.g. <tt>;</tt> or <tt>:<tt>.
+	 * 
+	 * @return the file separator.
+	 */
+	public String getPathSeparator() {
+		return pathSeparator;
+	}
+
+	/**
+	 * Returns the character used by the operating system family to separate components of a path, e.g. <tt>;</tt> or <tt>:<tt>.
+	 * 
+	 * @return the file separator.
+	 */
+	public char getPathSeparatorChar() {
+		return pathSeparatorChar;
+	}
+
+	/**
+	 * Returns the characters used by the operating system family to separate line in a text file, e.g. <tt>\r\n</tt> or <tt>\n</tt>
+	 * 
+	 * @return the line separator
+	 */
+	public String getLineSeparator() {
+		return lineSeparator;
+	}
+
+	/**
+	 * Returns the string used by the operating system family to separate commands in a command line, e.g. <tt>&&</tt> or <tt>;<tt>.
+	 * 
+	 * @return the command separator.
+	 */
+	public String getCommandSeparator() {
+		return commandSeparator;
+	}
+
+	/**
+	 * Returns the extension for scripts used by the operating system family, e.g. <tt>.bat</tt> or <tt>.sh</tt>
+	 * 
+	 * @return the script extension including the preceding dot
+	 */
+	public String getScriptExtension() {
+		return scriptExtension;
+	}
+
+	/**
+	 * Returns the default path of the temporary directory for this operating system family, i.e. <tt>C:\temp</tt> or <tt>/tmp</tt>.
+	 * 
+	 * @return the path
+	 */
+	public String getDefaultTemporaryDirectoryPath() {
+		return defaultTemporaryDirectoryPath;
 	}
 
 }
