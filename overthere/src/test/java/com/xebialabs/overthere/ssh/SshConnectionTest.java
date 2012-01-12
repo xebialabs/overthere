ba36b3fd681a64a7f16ba4f0696d0c3999ea98e9
Vincent Partington <vpartington@xebialabs.com>
2012-01-12 11:57:43 +0100

    Factored PresetClientSshConnection into a top-level class to be able to run JUnit tests on SshConnectionTest from Eclipse.

--- overthere/src/test/java/com/xebialabs/overthere/ssh/SshConnectionTest.java
+++ overthere/src/test/java/com/xebialabs/overthere/ssh/SshConnectionTest.java
@@ -1,16 +1,16 @@
 /*
  * This file is part of Overthere.
- * 
+ *
  * Overthere is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Overthere is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Overthere.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -23,7 +23,6 @@ import static com.xebialabs.overthere.ConnectionOptions.USERNAME;
 import static com.xebialabs.overthere.OperatingSystemFamily.UNIX;
 import static com.xebialabs.overthere.ssh.SshConnectionBuilder.CONNECTION_TYPE;
 import static com.xebialabs.overthere.ssh.SshConnectionBuilder.PRIVATE_KEY_FILE;
-import static com.xebialabs.overthere.ssh.SshConnectionBuilder.SSH_PROTOCOL;
 import static com.xebialabs.overthere.ssh.SshConnectionType.SFTP;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
@@ -37,23 +36,21 @@ import java.io.IOException;
 
 import net.schmizz.sshj.MockitoFriendlySSHClient;
 import net.schmizz.sshj.SSHClient;
-import net.schmizz.sshj.common.Factory;
 import net.schmizz.sshj.userauth.keyprovider.KeyProvider;
-
 import net.schmizz.sshj.userauth.method.AuthMethod;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Matchers;
 
 import com.xebialabs.overthere.ConnectionOptions;
-import com.xebialabs.overthere.OverthereFile;
-import com.xebialabs.overthere.RuntimeIOException;
 
 /**
  * Unit tests for {@link SshConnection}
  */
 public class SshConnectionTest {
-    private ConnectionOptions connectionOptions;
+
+	private ConnectionOptions connectionOptions;
 
     @Before
     public void init() {
@@ -63,14 +60,14 @@ public class SshConnectionTest {
         connectionOptions.set(ADDRESS, "nowhere.example.com");
         connectionOptions.set(USERNAME, "some-user");
     }
-    
+
     @Test
     public void passwordIsUsedIfNoKeyfile() throws IOException {
         SSHClient client = mockClient();
         String password = "secret";
         connectionOptions.set(PASSWORD, password);
         newConnectionWithClient(client).connect();
-        
+
         verify(client).auth(eq("some-user"), isA(AuthMethod.class), isA(AuthMethod.class));
     }
 
@@ -79,10 +76,10 @@ public class SshConnectionTest {
         SSHClient client = mockClient();
         connectionOptions.set(PRIVATE_KEY_FILE, "/path/to/keyfile");
         newConnectionWithClient(client).connect();
-        
+
         verify(client).authPublickey(eq("some-user"), Matchers.<KeyProvider>anyVararg());
     }
-    
+
     @Test
     public void keyfileOverridesPassword() throws IOException {
         SSHClient client = mockClient();
@@ -90,37 +87,20 @@ public class SshConnectionTest {
         connectionOptions.set(PASSWORD, password);
         connectionOptions.set(PRIVATE_KEY_FILE, "/path/to/keyfile");
         newConnectionWithClient(client).connect();
-        
+
         verify(client).authPublickey(eq("some-user"), Matchers.<KeyProvider>anyVararg());
         verify(client, never()).authPassword(anyString(), anyString());
     }
-    
+
     private static SSHClient mockClient() throws IOException {
         SSHClient client = mock(MockitoFriendlySSHClient.class);
         doNothing().when(client).connect("nowhere.example.com", 22);
         return client;
     }
-    
+
     private SshConnection newConnectionWithClient(SSHClient client) {
         return new PresetClientSshConnection(connectionOptions, client);
     }
-    
-    private static class PresetClientSshConnection extends SshConnection {
 
-        public PresetClientSshConnection(ConnectionOptions options, final SSHClient clientToReturn) {
-            super(SSH_PROTOCOL, options);
-            sshClientFactory = new Factory<SSHClient>() {
-                @Override
-                public SSHClient create() {
-                    return clientToReturn;
-                }
-            };
-        }
-
-        @Override
-        protected OverthereFile getFile(String hostPath, boolean isTempFile)
-                throws RuntimeIOException {
-            throw new UnsupportedOperationException("TODO Auto-generated method stub");
-        }
-    }
 }
+
