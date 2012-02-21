924fb06499d3eac078f4a67d30241337e6a2d650
Vincent Partington <vpartington@xebialabs.com>
2012-02-21 11:35:17 +0100

    Added explicit close() method that does not throw java.io.IOException

--- overthere/src/main/java/com/xebialabs/overthere/OverthereConnection.java
+++ overthere/src/main/java/com/xebialabs/overthere/OverthereConnection.java
@@ -1,6 +1,7 @@
 package com.xebialabs.overthere;
 
 import java.io.Closeable;
+import java.io.IOException;
 
 public interface OverthereConnection extends Closeable {
 	/**
@@ -95,6 +96,11 @@ public interface OverthereConnection extends Closeable {
 	 */
 	boolean canStartProcess();
 
+	/**
+	 * Closes the connection. Does not throw {@link IOException} but can throw {@link RuntimeIOException}
+	 */
+	void close();
+	
 	/**
 	 * Implementations MUST implement toString properly.
 	 */
