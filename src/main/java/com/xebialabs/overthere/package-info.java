f6944803bb82d4cc486c6265bd844e2a2b6724c9
hierynomus <jeroen@hierynomus.com>
2012-08-02 12:48:24 +0200

    Added License headers

--- src/main/java/com/xebialabs/overthere/package-info.java
+++ src/main/java/com/xebialabs/overthere/package-info.java
@@ -1,18 +1,24 @@
-/*
- * This file is part of Overthere.
- * 
- * Overthere is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * Overthere is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with Overthere.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Copyright (c) 2008, 2012, XebiaLabs B.V., All rights reserved.
+ *
+ *
+ * Overthere is licensed under the terms of the GPLv2
+ * <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most XebiaLabs Libraries.
+ * There are special exceptions to the terms and conditions of the GPLv2 as it is applied to
+ * this software, see the FLOSS License Exception
+ * <http://github.com/xebialabs/overthere/blob/master/LICENSE>.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth
+ * Floor, Boston, MA 02110-1301  USA
  */
 /**
  * Contains the core API of the Overthere library. This library allows users to work with local or remote systems through an abstraction layer. Some of the features:
@@ -23,7 +29,7 @@
  * <li>A remote system can be accessed using any number of connection methods implemented: SSH, CIFS/Telnet, etc.</li>
  * <li>The host can be a Windows or a Unix machine.</li>
  * </ul>
- * 
+ *
  * To start, create a {@link com.xebialabs.overthere.OverthereConnection connection} by invoking {@link com.xebialabs.overthere.Overthere#getConnection(String, ConnectionOptions)}.
  * Then, create file objects using {@link com.xebialabs.overthere.OverthereConnection#getFile(String)} and manipulate them or execute commands using
  * {@link com.xebialabs.overthere.OverthereConnection#execute(OverthereProcessOutputHandler, CmdLine)}).
