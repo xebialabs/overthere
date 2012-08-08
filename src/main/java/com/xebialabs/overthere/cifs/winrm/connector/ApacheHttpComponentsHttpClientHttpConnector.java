xebialabs/overthere

commit 794080c31d988f012b0757b7b835d22ab0ff2992

Prevented "Unable to load realm info from SCDynamicStore" from showing up when not using Kerberos authentication.

src/main/java/com/xebialabs/overthere/cifs/winrm/connector/ApacheHttpComponentsHttpClientHttpConnector.java
@@ -25,7 +25,6 @@ package com.xebialabs.overthere.cifs.winrm.connector;
 import static org.apache.http.auth.AuthScope.ANY_HOST;
 import static org.apache.http.auth.AuthScope.ANY_PORT;
 import static org.apache.http.auth.AuthScope.ANY_REALM;
-import static org.apache.http.auth.AuthScope.ANY_SCHEME;
 import static org.apache.http.client.params.ClientPNames.HANDLE_AUTHENTICATION;
 import static org.apache.http.conn.ssl.SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
 import static org.apache.http.conn.ssl.SSLSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER;
@@ -66,6 +65,7 @@ import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.BasicUserPrincipal;
 import org.apache.http.auth.Credentials;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.scheme.Scheme;
@@ -87,7 +87,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.io.Closeables;
-import com.sun.security.auth.UserPrincipal;
 import com.xebialabs.overthere.cifs.WinrmHttpsCertificateTrustStrategy;
 import com.xebialabs.overthere.cifs.WinrmHttpsHostnameVerificationStrategy;
 import com.xebialabs.overthere.cifs.winrm.HttpConnector;
@@ -116,10 +115,11 @@ public class ApacheHttpComponentsHttpClientHttpConnector implements HttpConnecto
      */
     @Override
     public Document sendMessage(final Document requestDocument, final SoapAction soapAction) {
-        if (username.contains("@")) {
+        boolean isDomainUser = username.contains("@");
+        if (isDomainUser) {
             return runPrivileged(new PrivilegedSendMessage(this, requestDocument, soapAction));
         } else {
-            return int_sendMessage(requestDocument, soapAction);
+            return doSendMessage(requestDocument, soapAction);
         }
     }
 
@@ -221,7 +221,7 @@ public class ApacheHttpComponentsHttpClientHttpConnector implements HttpConnecto
 
         @Override
         public Document run() throws Exception {
-            return connector.int_sendMessage(requestDocument, soapAction);
+            return connector.doSendMessage(requestDocument, soapAction);
         }
 
         public Document getRequestDocument() {
@@ -232,7 +232,7 @@ public class ApacheHttpComponentsHttpClientHttpConnector implements HttpConnecto
     /**
      * Internal sendMessage, performs the HTTP request and returns the result document.
      */
-    private Document int_sendMessage(final Document requestDocument, final SoapAction soapAction) {
+    private Document doSendMessage(final Document requestDocument, final SoapAction soapAction) {
         final DefaultHttpClient client = new DefaultHttpClient();
         try {
             configureHttpClient(client);
@@ -296,9 +296,9 @@ public class ApacheHttpComponentsHttpClientHttpConnector implements HttpConnecto
      * Configure auth schemes to use for the HttpClient.
      */
     protected void configureAuthentication(final DefaultHttpClient httpclient) {
-        httpclient.getCredentialsProvider().setCredentials(new AuthScope(ANY_HOST, ANY_PORT, ANY_REALM, ANY_SCHEME), new Credentials() {
+        httpclient.getCredentialsProvider().setCredentials(new AuthScope(ANY_HOST, ANY_PORT, ANY_REALM, "basic"), new Credentials() {
             public Principal getUserPrincipal() {
-                return new UserPrincipal(username);
+                return new BasicUserPrincipal(username);
             }
 
             public String getPassword() {
@@ -405,7 +405,7 @@ public class ApacheHttpComponentsHttpClientHttpConnector implements HttpConnecto
     protected HttpEntity createEntity(final String requestDocAsString) throws UnsupportedEncodingException {
         return new StringEntity(requestDocAsString, ContentType.create("application/soap+xml", "UTF-8"));
     }
-
+    
     public URL getTargetURL() {
         return targetURL;
     }

src/main/java/com/xebialabs/overthere/cifs/winrm/WinRmClient.java
@@ -138,7 +138,7 @@ public class WinRmClient {
             if (chunk == 0) {
                 try {
                     exitCode = getFirstElement(responseDocument, ResponseExtractor.EXIT_CODE);
-                    logger.info("exit code {}", exitCode);
+                    logger.debug("exit code {}", exitCode);
                 } catch (Exception e) {
                     logger.debug("not found");
                 }

commit 03cc9a90db9071499e1a78afe133c96177d93caf

Fixed Kerberos authentication broken by commit 794080c31d988f012b0757b7b835d22ab0ff2992.

src/main/java/com/xebialabs/overthere/cifs/winrm/connector/ApacheHttpComponentsHttpClientHttpConnector.java
@@ -56,6 +56,7 @@ import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.NameCallback;
 import javax.security.auth.callback.PasswordCallback;
 import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.kerberos.KerberosPrincipal;
 import javax.security.auth.login.AppConfigurationEntry;
 import javax.security.auth.login.Configuration;
 import javax.security.auth.login.LoginContext;
@@ -98,6 +99,7 @@ import com.xebialabs.overthere.cifs.winrm.soap.SoapAction;
 public class ApacheHttpComponentsHttpClientHttpConnector implements HttpConnector {
     private static Logger logger = LoggerFactory.getLogger(ApacheHttpComponentsHttpClientHttpConnector.class);
     private final String username;
+    private final boolean useKerberos;
     private final String password;
     private final URL targetURL;
     private WinrmHttpsCertificateTrustStrategy httpsCertTrustStrategy;
@@ -106,6 +108,7 @@ public class ApacheHttpComponentsHttpClientHttpConnector implements HttpConnecto
 
     public ApacheHttpComponentsHttpClientHttpConnector(final String username, final String password, final URL targetURL) {
         this.username = username;
+        this.useKerberos = username.contains("@");
         this.password = password;
         this.targetURL = targetURL;
     }
@@ -115,8 +118,7 @@ public class ApacheHttpComponentsHttpClientHttpConnector implements HttpConnecto
      */
     @Override
     public Document sendMessage(final Document requestDocument, final SoapAction soapAction) {
-        boolean isDomainUser = username.contains("@");
-        if (isDomainUser) {
+        if (useKerberos) {
             return runPrivileged(new PrivilegedSendMessage(this, requestDocument, soapAction));
         } else {
             return doSendMessage(requestDocument, soapAction);
@@ -296,7 +298,7 @@ public class ApacheHttpComponentsHttpClientHttpConnector implements HttpConnecto
      * Configure auth schemes to use for the HttpClient.
      */
     protected void configureAuthentication(final DefaultHttpClient httpclient) {
-        httpclient.getCredentialsProvider().setCredentials(new AuthScope(ANY_HOST, ANY_PORT, ANY_REALM, "basic"), new Credentials() {
+        httpclient.getCredentialsProvider().setCredentials(new AuthScope(ANY_HOST, ANY_PORT, ANY_REALM, "Basic"), new Credentials() {
             public Principal getUserPrincipal() {
                 return new BasicUserPrincipal(username);
             }
@@ -305,6 +307,18 @@ public class ApacheHttpComponentsHttpClientHttpConnector implements HttpConnecto
                 return password;
             }
         });
+
+        if (useKerberos) {
+            httpclient.getCredentialsProvider().setCredentials(new AuthScope(ANY_HOST, ANY_PORT, ANY_REALM, "Kerberos"), new Credentials() {
+                public Principal getUserPrincipal() {
+                    return new KerberosPrincipal(username);
+                }
+
+                public String getPassword() {
+                    return password;
+                }
+            });
+        }
 
         httpclient.getParams().setBooleanParameter(HANDLE_AUTHENTICATION, true);
     }