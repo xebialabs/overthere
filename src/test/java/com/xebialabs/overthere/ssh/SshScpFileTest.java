2847573aa6820797a885d4846185a5da45f7a5ba
hierynomus <jeroen@javadude.nl>
2012-12-17 15:47:36 +0100

    Added tests for different permissions on SshScpFile

--- src/test/java/com/xebialabs/overthere/ssh/SshScpFileTest.java
+++ src/test/java/com/xebialabs/overthere/ssh/SshScpFileTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
 import static org.mockito.Mockito.mock;
 
 public class SshScpFileTest {
@@ -30,6 +31,22 @@ public class SshScpFileTest {
         SshScpFile.LsResults results = new SshScpFile.LsResults();
         boolean b = sshScpFile.parseLsOutputLine(results, "drwxr-xr-x@ 10 ajvanerp  staff    340 Dec 17 15:28 build");
         assertThat("Should be a directory", results.isDirectory);
+        assertThat("Should be executable", results.canExecute);
+    }
+
+    @Test
+    public void shouldParseDirectoryWithEndingDotOnLs() {
+        SshScpFile.LsResults results = new SshScpFile.LsResults();
+        boolean b = sshScpFile.parseLsOutputLine(results, "drwxr-xr-x. 10 ajvanerp  staff    340 Dec 17 15:28 build");
+        assertThat("Should be a directory", results.isDirectory);
+        assertThat("Should be executable", results.canExecute);
+    }
+
+    @Test
+    public void shouldParseSymLinkOnLs() {
+        SshScpFile.LsResults results = new SshScpFile.LsResults();
+        boolean b = sshScpFile.parseLsOutputLine(results, "lrwxr-xr-x 10 ajvanerp  staff    340 Dec 17 15:28 build");
+        assertThat("Should be a directory", !results.isDirectory);
         assertThat("Should be executable", results.canExecute);
     }
 }
