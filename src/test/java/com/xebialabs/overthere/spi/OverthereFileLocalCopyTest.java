4c31d778db2341aaabb0f07474eaf5c0e7aaf928
Ric Klaren <rklaren@xebialabs.com>
2015-10-06 15:24:37 +0200

    Update license headers

--- src/test/java/com/xebialabs/overthere/spi/OverthereFileLocalCopyTest.java
+++ src/test/java/com/xebialabs/overthere/spi/OverthereFileLocalCopyTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2008-2014, XebiaLabs B.V., All rights reserved.
+ * Copyright (c) 2008-2015, XebiaLabs B.V., All rights reserved.
  *
  *
  * Overthere is licensed under the terms of the GPLv2
