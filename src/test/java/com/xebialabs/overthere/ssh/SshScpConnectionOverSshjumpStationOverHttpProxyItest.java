093f29d5253b6ed136d1c9708b8d4de4be777849
Vincent Partington <vpartington@xebialabs.com>
2016-03-11 18:09:08 +0100

    Added integration tests for HTTP proxy and SSH jumpstation support.

---
+++ src/test/java/com/xebialabs/overthere/ssh/SshScpConnectionOverSshjumpStationOverHttpProxyItest.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2008-2015, XebiaLabs B.V., All rights reserved.
+ *
+ *
+ * Overthere is licensed under the terms of the GPLv2
+ * <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most XebiaLabs Libraries.
+ * There are special exceptions to the terms and conditions of the GPLv2 as it is applied to
+ * this software, see the FLOSS License Exception
+ * <http://github.com/xebialabs/overthere/blob/master/LICENSE>.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth
+ * Floor, Boston, MA 02110-1301  USA
+ */
+package com.xebialabs.overthere.ssh;
+
+import com.xebialabs.overthere.ConnectionOptions;
+import com.xebialabs.overthere.UnixCloudHostListener;
+import com.xebialabs.overthere.WindowsCloudHostListener;
+import com.xebialabs.overthere.itest.OverthereConnectionItestBase;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import static com.xebialabs.overthere.ConnectionOptions.*;
+import static com.xebialabs.overthere.OperatingSystemFamily.UNIX;
+import static com.xebialabs.overthere.OperatingSystemFamily.WINDOWS;
+import static com.xebialabs.overthere.WindowsCloudHostListener.ADMINISTRATIVE_USER_ITEST_PASSWORD;
+import static com.xebialabs.overthere.WindowsCloudHostListener.ADMINISTRATIVE_USER_ITEST_USERNAME;
+import static com.xebialabs.overthere.proxy.ProxyConnection.PROXY_PROTOCOL;
+import static com.xebialabs.overthere.proxy.ProxyConnection.PROXY_TYPE;
+import static com.xebialabs.overthere.ssh.SshConnectionBuilder.*;
+import static com.xebialabs.overthere.ssh.SshConnectionType.SCP;
+import static com.xebialabs.overthere.ssh.SshTestUtils.createPrivateKeyFile;
+import static java.net.Proxy.Type.HTTP;
+
+@Test
+@Listeners({UnixCloudHostListener.class, WindowsCloudHostListener.class})
+public class SshScpConnectionOverSshjumpStationOverHttpProxyItest extends OverthereConnectionItestBase {
+
+    @Override
+    protected String getProtocol() {
+        return SSH_PROTOCOL;
+    }
+
+    @Override
+    protected ConnectionOptions getOptions() {
+        ConnectionOptions proxyOptions = new ConnectionOptions();
+        proxyOptions.set(PROTOCOL, PROXY_PROTOCOL);
+        proxyOptions.set(PROXY_TYPE, HTTP);
+        proxyOptions.set(ADDRESS, UnixCloudHostListener.getHost().getHostName());
+        proxyOptions.set(PORT, 8888);
+
+        ConnectionOptions jumpstationOptions = new ConnectionOptions();
+        jumpstationOptions.set(OPERATING_SYSTEM, WINDOWS);
+        jumpstationOptions.set(ADDRESS, WindowsCloudHostListener.getHost().getHostName());
+        jumpstationOptions.set(PORT, 22);
+        jumpstationOptions.set(USERNAME, ADMINISTRATIVE_USER_ITEST_USERNAME);
+        jumpstationOptions.set(PASSWORD, ADMINISTRATIVE_USER_ITEST_PASSWORD);
+        jumpstationOptions.set(JUMPSTATION, proxyOptions);
+
+        ConnectionOptions options = new ConnectionOptions();
+        options.set(OPERATING_SYSTEM, UNIX);
+        options.set(CONNECTION_TYPE, SCP);
+        options.set(ADDRESS, UnixCloudHostListener.getHost().getHostName());
+        options.set(PORT, 22);
+        options.set(USERNAME, "overthere");
+        options.set(PASSWORD, "}Kp39hdb}LzM");
+        options.set(JUMPSTATION, jumpstationOptions);
+        return options;
+    }
+
+    @Override
+    protected String getExpectedConnectionClassName() {
+        return SshScpConnection.class.getName();
+    }
+
+}
---
+++ src/test/java/com/xebialabs/overthere/ssh/SshSftpWinsshdConnectionWithRegularUserOverHttpProxyItest.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2008-2015, XebiaLabs B.V., All rights reserved.
+ *
+ *
+ * Overthere is licensed under the terms of the GPLv2
+ * <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most XebiaLabs Libraries.
+ * There are special exceptions to the terms and conditions of the GPLv2 as it is applied to
+ * this software, see the FLOSS License Exception
+ * <http://github.com/xebialabs/overthere/blob/master/LICENSE>.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth
+ * Floor, Boston, MA 02110-1301  USA
+ */
+package com.xebialabs.overthere.ssh;
+
+import com.xebialabs.overthere.ConnectionOptions;
+import com.xebialabs.overthere.UnixCloudHostListener;
+import com.xebialabs.overthere.WindowsCloudHostListener;
+import com.xebialabs.overthere.itest.OverthereConnectionItestBase;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import static com.xebialabs.overthere.ConnectionOptions.*;
+import static com.xebialabs.overthere.OperatingSystemFamily.WINDOWS;
+import static com.xebialabs.overthere.WindowsCloudHostListener.REGULAR_USER_ITEST_PASSWORD;
+import static com.xebialabs.overthere.WindowsCloudHostListener.REGULAR_USER_ITEST_USERNAME;
+import static com.xebialabs.overthere.proxy.ProxyConnection.PROXY_PROTOCOL;
+import static com.xebialabs.overthere.proxy.ProxyConnection.PROXY_TYPE;
+import static com.xebialabs.overthere.ssh.SshConnectionBuilder.*;
+import static com.xebialabs.overthere.ssh.SshConnectionType.SFTP_WINSSHD;
+import static java.net.Proxy.Type.HTTP;
+
+@Test
+@Listeners({UnixCloudHostListener.class, WindowsCloudHostListener.class})
+public class SshSftpWinsshdConnectionWithRegularUserOverHttpProxyItest extends OverthereConnectionItestBase {
+
+    @Override
+    protected String getProtocol() {
+        return SSH_PROTOCOL;
+    }
+
+    @Override
+    protected ConnectionOptions getOptions() {
+        ConnectionOptions proxyOptions = new ConnectionOptions();
+        proxyOptions.set(PROTOCOL, PROXY_PROTOCOL);
+        proxyOptions.set(PROXY_TYPE, HTTP);
+        proxyOptions.set(ADDRESS, UnixCloudHostListener.getHost().getHostName());
+        proxyOptions.set(PORT, 8888);
+
+        ConnectionOptions options = new ConnectionOptions();
+        options.set(OPERATING_SYSTEM, WINDOWS);
+        options.set(CONNECTION_TYPE, SFTP_WINSSHD);
+        options.set(ADDRESS, WindowsCloudHostListener.getHost().getHostName());
+        options.set(PORT, 2222);
+        options.set(USERNAME, REGULAR_USER_ITEST_USERNAME);
+        options.set(PASSWORD, REGULAR_USER_ITEST_PASSWORD);
+        options.set(ALLOCATE_PTY, "xterm:80:24:0:0");
+        options.set(JUMPSTATION, proxyOptions);
+        return options;
+    }
+
+    @Override
+    protected String getExpectedConnectionClassName() {
+        return SshSftpWinSshdConnection.class.getName();
+    }
+
+}
