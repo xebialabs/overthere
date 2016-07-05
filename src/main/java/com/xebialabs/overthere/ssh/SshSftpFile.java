b2809c91c164f76c6e7254f9648072d622ad6772
Jeroen van Erp <jeroen@hierynomus.com>
2016-07-05 13:54:26 +0200

    Upgraded to SSHj 0.17.0 and fixed #182

--- src/main/java/com/xebialabs/overthere/ssh/SshSftpFile.java
+++ src/main/java/com/xebialabs/overthere/ssh/SshSftpFile.java
@@ -1,21 +1,21 @@
 /**
  * Copyright (c) 2008-2016, XebiaLabs B.V., All rights reserved.
- *
- *
+ * <p>
+ * <p>
  * Overthere is licensed under the terms of the GPLv2
  * <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most XebiaLabs Libraries.
  * There are special exceptions to the terms and conditions of the GPLv2 as it is applied to
  * this software, see the FLOSS License Exception
  * <http://github.com/xebialabs/overthere/blob/master/LICENSE>.
- *
+ * <p>
  * This program is free software; you can redistribute it and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software Foundation; version 2
  * of the License.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License along with this
  * program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth
  * Floor, Boston, MA 02110-1301  USA
@@ -140,15 +140,7 @@ class SshSftpFile extends SshFile<SshSftpConnection> {
     protected void copyFrom(OverthereFile source) {
         SFTPFileTransfer fileTransfer = connection.getSharedSftpClient().getFileTransfer();
         try {
-            // If the source is a directory and the name and the target name do not match, SFTP copies the
-            // directory into the destination, whereas we want to copy the contents into the destination.
-            if (source.isDirectory() && this.exists() && !source.getName().equals(this.getName())) {
-                for (OverthereFile overthereFile : source.listFiles()) {
-                    fileTransfer.upload(new OverthereFileLocalSourceFile(overthereFile), getSftpPath());
-                }
-            } else {
-                fileTransfer.upload(new OverthereFileLocalSourceFile(source), getSftpPath());
-            }
+            fileTransfer.upload(new OverthereFileLocalSourceFile(source), getSftpPath());
         } catch (IOException ioe) {
             throw new RuntimeIOException(format("Cannot upload %s to %s", source, this), ioe);
         }
