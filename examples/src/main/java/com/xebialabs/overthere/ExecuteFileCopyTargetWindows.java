dc5a05bbb380c035f6f61e61242e1c1d25599969
arulprasad-ai <arul.kathiresan@digital.ai>
2024-09-04 13:47:24 +0530

    D-35186 java client Test added in overthere for filecopy

--- examples/src/main/java/com/xebialabs/overthere/ExecuteFileCopyTargetWindows.java
+++ examples/src/main/java/com/xebialabs/overthere/ExecuteFileCopyTargetWindows.java
@@ -1,22 +1,15 @@
 package com.xebialabs.overthere;
 
-import static com.xebialabs.overthere.ConnectionOptions.*;
-import static com.xebialabs.overthere.OperatingSystemFamily.WINDOWS;
-import static com.xebialabs.overthere.cifs.BaseCifsConnectionBuilder.PATH_SHARE_MAPPINGS;
-import static com.xebialabs.overthere.cifs.CifsConnectionBuilder.CONNECTION_TYPE;
-import static com.xebialabs.overthere.cifs.CifsConnectionType.WINRM_INTERNAL;
-import static com.xebialabs.overthere.cifs.CifsConnectionType.WINRM_NATIVE;
 
-import com.google.common.collect.ImmutableMap;
-import com.xebialabs.overthere.CmdLine;
-import com.xebialabs.overthere.ConnectionOptions;
-import com.xebialabs.overthere.Overthere;
-import com.xebialabs.overthere.OverthereConnection;
+import static com.xebialabs.overthere.ConnectionOptions.*;
+import static com.xebialabs.overthere.ConnectionOptions.OPERATING_SYSTEM;
+import static com.xebialabs.overthere.OperatingSystemFamily.WINDOWS;
+import static com.xebialabs.overthere.cifs.BaseCifsConnectionBuilder.CONNECTION_TYPE;
+import static com.xebialabs.overthere.cifs.CifsConnectionType.WINRM_INTERNAL;
 
 public class ExecuteFileCopyTargetWindows {
 
 	public static void main(String[] args) {
-
 		ConnectionOptions options = new ConnectionOptions();
 		options.set(ADDRESS, "172.18.60.118");
 		options.set(USERNAME, "Administrator");
@@ -25,16 +18,40 @@ public class ExecuteFileCopyTargetWindows {
 		options.set(CONNECTION_TYPE, WINRM_INTERNAL	);
 		//options.set(FILE_COPY_COMMAND_FOR_WINDOWS, "copy {0} {1}");
 		//options.set(PATH_SHARE_MAPPINGS, ImmutableMap.of("c:\\foltest\\arulshare", "foltest\\arulshare"))	;
-		//ConnectionOptions optionsLocal = new ConnectionOptions();
-		//OverthereConnection connLocal = Overthere.getConnection("local", optionsLocal);
 
-		OverthereConnection connection = Overthere.getConnection("smb", options);
+		testfileCopyinTargetWinowsMachine(options);
+		testfileCopyinTargetWinowsMachineNoShare(options);
+		testfileTransmitFromlocaltoTargetWindowsMachine(options);
+	}
 
-
+	private static void testfileCopyinTargetWinowsMachine(ConnectionOptions targetOptions) {
+		OverthereConnection connection = Overthere.getConnection("smb", targetOptions);
 		try {
-			//	connection.execute(CmdLine.build("type", "\\windows\\system32\\drivers\\etc\\hosts"));
-			//	connection.execute(CmdLine.build("ipconfig", "/all"));
 			OverthereFile from = connection.getFile("C:\\foltest\\arulshare\\makeLocalCpy.log");
+			OverthereFile to = connection.getFile("C:\\foltest\\arulshare\\makeLocalCpy2.log");
+			from.copyTo(to);
+		} finally {
+			connection.close();
+		}
+	}
+
+	private static void testfileCopyinTargetWinowsMachineNoShare(ConnectionOptions targetOptions) {
+		OverthereConnection connection = Overthere.getConnection("smb", targetOptions);
+		try {
+			OverthereFile from = connection.getFile("C:\\arulnoshare\\makenoshare.log");
+			OverthereFile to = connection.getFile("C:\\arulnoshare\\makenoshareCopy2.log");
+			from.copyTo(to);
+		} finally {
+			connection.close();
+		}
+	}
+
+	private static void testfileTransmitFromlocaltoTargetWindowsMachine(ConnectionOptions targetOptions) {
+		ConnectionOptions optionsLocal = new ConnectionOptions();
+		OverthereConnection connLocal = Overthere.getConnection("local", optionsLocal);
+		OverthereConnection connection = Overthere.getConnection("smb", targetOptions);
+		try {
+			OverthereFile from = connLocal.getFile("/Users/arulprasad/projects/2024/TestResults/make.log");
 			OverthereFile to = connection.getFile("C:\\foltest\\arulshare\\makeLocalCpy2.log");
 			from.copyTo(to);
 		} finally {
